67294017d99f2f102429c33781d693db8f91b1e4
Tiago Ary Castelo <51384842+springfield2002@users.noreply.github.com>
2020-10-30 12:12:09 -0300

    alterações no personagem

--- springfield2002-TURBO-HYPER-ULTRA-MEGA-LASER-NINJA-3000-fbada66/GAMEJAM/Instances/Main Character/playerController.gd
+++ springfield2002-TURBO-HYPER-ULTRA-MEGA-LASER-NINJA-3000-fbada66/GAMEJAM/Instances/Main Character/playerController.gd
@@ -7,6 +7,7 @@ export var jump = 300
 export var gravity = 10
 var second_jump = 3
 var dano = 0
+var attack = 0
 export var life = 5
 signal hit
 var eixo_horizontal
@@ -17,10 +18,16 @@ func _physics_process(delta):
 	print(movement.y)
 	if !is_on_floor():
 		movement.y += gravity
-		if movement.y > 0:
-				$AnimatedSprite.play("fall")
+		if attack == 1:
+			$AnimatedSprite.rotation_degrees += $AnimatedSprite.scale.x * 37.5
+		if movement.y > 0 && attack == 0:
+			$AnimatedSprite.play("fall")
+
 	else:
 		second_jump = 3
+		attack = 0
+		$AnimationPlayer.stop(true)
+		$AnimatedSprite.rotation_degrees = 0
 	if Input.is_action_just_pressed("jump"):	
 		if is_on_floor():
 			movement.y = -jump
@@ -29,7 +36,7 @@ func _physics_process(delta):
 			movement.y = -jump
 			second_jump  -= 1
 	
-	eixo_horizontal =   Input.get_action_strength("right") - Input.get_action_strength("left")
+	eixo_horizontal = Input.get_action_strength("right") - Input.get_action_strength("left")
 	movement.x = eixo_horizontal * speed
 	
 	if is_on_ceiling():
@@ -42,9 +49,9 @@ func _physics_process(delta):
 	
 func update_animations():
 	if movement.x > 0:
-		$AnimatedSprite.scale.x = 1.5
+		$AnimatedSprite.scale.x = 1.2
 	elif movement.x < 0:
-		$AnimatedSprite.scale.x = -1.5
+		$AnimatedSprite.scale.x = -1.2
 	if is_on_floor():
 		if abs(movement.x) > 0:
 			if dano != 1:
@@ -55,10 +62,14 @@ func update_animations():
 		
 	
 		
-			if movement.y < 0:
-				$AnimatedSprite.play("jump")
+	if Input.is_action_pressed("jump"):
+		$AnimatedSprite.play("jump")
 			
+	if Input.is_action_pressed("attack"):
+		attack = 1
+		if !is_on_floor():
 			
-
+			$AnimationPlayer.play("airslash")
+			
 
 
